45a8d73774e65432697349c0e89fe1375c80059e
Sandy <etozhesandy@gmail.com>
2024-06-13 13:41:02 +0300

    Initial commit

---
+++ app/src/main/java/su/afk/cleancode/MainActivity.kt
@@ -0,0 +1,59 @@
+package su.afk.cleancode
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import su.afk.cleancode.ui.theme.CleanCodeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleanCodeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = { /*TODO*/ }) {
+
+        }
+        Button(onClick = { /*TODO*/ }) {
+
+        }
+    }
+
+}
+// чистый код с авторизацией
+//https://dummyjson.com/docs/auth
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    CleanCodeTheme {
+        Greeting("Android")
+    }
+}
